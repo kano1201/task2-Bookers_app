89ecefa3e4cca4e3f8dab24d20c9d323d02fd732
kano1201 <kano.nakamura3@gmail.com>
2021-06-08 08:00:01 +0000

    [Add]一覧show,edit,destroyボタン

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,13 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-
+  
   def new
     @book = Book.new
   end
   
   def create
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     book.save
     redirect_to book_path(book.id)
   end
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
   
-  private
+   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
