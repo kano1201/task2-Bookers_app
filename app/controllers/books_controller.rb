eb7371c5a0a94eb264a298e38e57ab63346f5c2b
kano1201 <kano.nakamura3@gmail.com>
2021-06-10 03:44:53 +0000

    [Add]successメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,12 +7,18 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+    
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+     flash[:notice] = "successful"
+     redirect_to book_path(@book.id)
+    else
+      flash.now[:danger] = "error"
+      render :index
+    end
   end
 
   def edit
